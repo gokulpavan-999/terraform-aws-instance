31cd9e63f63ed0896486aa08da97eb72e9fc741a
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-22 13:04:19 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "public_ip" {
   value = aws_instance.this.public_ip
   description = "public IP of the instance created"
 }
+
+output "private_ip" {
+  value = aws_instance.this.private_ip
+  description = "private IP of the instance created"
+}
+
+output "instance_ip" {
+  value = aws_instance.this.instance_ip
+  description = "instance IP of the instance created"
+}
